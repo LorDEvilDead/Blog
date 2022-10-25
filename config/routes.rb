a06cec3f4f3bd3fea8ddb97a7b829aa87a58bf32
Pavel <lordevildead@gmail.com>
2022-10-25 22:58:22 +0300

    generate controller to comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
-
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles do 
+    resources :comments
+  end
 end
+
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#end
