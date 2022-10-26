b04644b45713ecc4af1655f7fd4ee0103587d7ed
Pavel <lordevildead@gmail.com>
2022-10-26 17:27:23 +0300

    statuses

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,6 @@ class CommentsController < ApplicationController
 
 private
     def comment_params
-    params.require(:comment).permit(:commenter, :dody, :status)
+    params.require(:comment).permit(:commenter, :body, :status)
     end
 end
